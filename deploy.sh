3b69ae2023ac119efcb7c846a1d4adf0b593eb1a
Josh Ternyak <josh@brandonfire.com>
2020-06-14 13:19:57 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 rm -rf docs
-wget --mirror --convert-links --html-extension --wait=0 --no-cache http://localhost:2369 --directory-prefix=docs --no-host-directories
+wget --mirror --convert-links --html-extension --wait=0 --no-cache
+http://localhost:2369 --directory-prefix=docs --no-host-directories --content-disposition
 cd docs && find . -type f | xargs perl -pi~ -e 's/localhost:2369/joshternyak.com/g;' && cd ..
 cd docs && find . -type f | xargs perl -pi~ -e 's/http:/https:/g;' && cd ..
 git add -A
 git commit -m "deploy"
 git push origin master
-for i in `find $1 -type f`
-do
-    mv $i `echo $i | cut -d? -f1`
-done
